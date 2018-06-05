83bcbfbdc31e472059b83cccc9e0ae93c1029b37
jadychan <jadychan123@gmail.com>
2018-06-05 05:55:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,13 @@ def fizzbuzz(num)
   if num % 3 == 0 # if the number int is divisible by 3
     puts "Fizz" # Go fizz
   end
-
+  if num % 5 == 0
+    puts "Buzz"
+  end
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  end
+  puts nil
 end
 
 fizzbuzz(3)
