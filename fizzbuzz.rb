6c724880b4071c206735e51a4788eff5d1051d72
jadychan <jadychan123@gmail.com>
2018-06-05 05:59:24 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  end
+  if num % 3 == 0 # if the number int is divisible by 3
+    return "Fizz" # Go fizz
+  end
+  if num % 5 == 0
+    return "Buzz"
+  end
+  return nil
+end
