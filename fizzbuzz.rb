3898c6eeae119cdc1d55619146acb2275680ce06
jadychan <jadychan123@gmail.com>
2018-06-05 05:57:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(num)
   if num % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    return "Fizz" # Go fizz
   end
   if num % 5 == 0
-    "Buzz"
+    return "Buzz"
   end
   if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
+    return "FizzBuzz"
   end
   nil
 end
